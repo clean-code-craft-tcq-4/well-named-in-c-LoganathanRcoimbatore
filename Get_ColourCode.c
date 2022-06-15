987c10311dea25ef5ce902c634de4634fac7e220
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-06-15 13:03:34 +0530

    Update Get_ColourCode.c

--- Get_ColourCode.c
+++ Get_ColourCode.c
@@ -1,17 +1,13 @@
-#include "main.h.h"
-#include "Get_ColourCode.h"
+#include "ColourCode_if.h"
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+const char* majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int numberOfMajorColors = sizeof(majorColors) / sizeof(majorColors[0]);
+int numberOfMinorColors = sizeof(minorColors) / sizeof(minorColors[0]);
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) 
 {
-    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
+    sprintf(buffer, "%s %s",majorColors[colorPair->majorColor],minorColors[colorPair->minorColor]);
 }
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
@@ -24,4 +20,24 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors + colorPair->minorColor + 1;
+}
+
+void Display_AllPair_ColorCode(void)
+{
+    int MajorColor_Index;
+    int MinorColor_Index;
+    int PairNumber = 1;
+
+
+    printf("| Pair No. | Major Color | Minor Color |");
+    printf("\n---------------------------------------- \n");
+    for(MajorColor_Index = 0; MajorColor_Index < numberOfMajorColors; MajorColor_Index++)
+    {
+        for(MinorColor_Index = 0; MinorColor_Index < numberOfMinorColors; MinorColor_Index++)
+        {
+            printf("|   %d \t  |  %s\t|  %s\t|\n",PairNumber++, majorColors[MajorColor_Index], minorColors[MinorColor_Index]);
+        }
+        printf("\n---------------------------------------- \n");
+    }
+
 }
