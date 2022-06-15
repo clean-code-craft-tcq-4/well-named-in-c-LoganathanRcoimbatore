4ae21fd7571789b4316a13c8512c9884194d4a5e
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-06-15 13:04:12 +0530

    Update Test_ColourCode.c

--- Test_ColourCode.c
+++ Test_ColourCode.c
@@ -1,8 +1,6 @@
-#include "main.h"
-#include "Get_ColourCode.h"
-#include "Test_ColourCode.h"
+#include "ColourCode_if.h"s
 
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor)
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
@@ -12,7 +10,7 @@ void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorCol
     assert(colorPair.minorColor == expectedMinor);
 }
 
-void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
 {
     ColorPair colorPair;
     colorPair.majorColor = major;
