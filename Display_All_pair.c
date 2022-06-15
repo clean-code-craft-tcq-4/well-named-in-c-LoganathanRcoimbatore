3a6b21f30d91261e4802e2492e9ae92da3e812ba
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-06-15 12:24:54 +0530

    Update Display_All_pair.c

--- Display_All_pair.c
+++ Display_All_pair.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include"main.h"
 #include "Display_All_pair.h"
 
 void Display_AllPair_ColorCode(void)
 {
-	char ColorPair_Names[MAX_COLORPAIR_NAME_CHARS];
-	int Pair_Number;
+    int MajorColor_Index;
+    int MinorColor_Index;
+    int Pair_Number = 1;
 
-	printf("\nAll pair color code with PairNumber, MajorColor & MinorColor\n");
-	
-	for (Pair_Number = 1; Pair_Number <= MAX_PAIR_NUMBER; Pair_Number++)
-	{
-		ColorPair colorPair = GetColorFromPairNumber(Pair_Number);
-		ColorPairToString(&colorPair, ColorPair_Names);
-		printf("%d %s\n", Pair_Number, ColorPair_Names);
-	}
+    printf("\nAll pair Color Codes Reference manual \n");
+    printf("\n");
+    printf("| Pair No. | Major Color | Minor Color |");
+    printf("\n");
+    for(MajorColor_Index = 0; MajorColor_Index < numberOfMajorColors; MajorColor_Index++)
+    {
+        for(MinorColor_Index = 0; MinorColor_Index < numberOfMinorColors; MinorColor_Index++)
+        {
+            printf("|   %d \t  |  %s\t|  %s\t|\n",Pair_Number++, majorColors[MajorColor_Index], minorColors[MinorColor_Index]);
+        }
+        printf("\n");
+    }
+    
 }
