941aad06b9142137b373e1feec6b618f19c9df82
LOGANATHAN <105205462+LoganathanRcoimbatore@users.noreply.github.com>
2022-06-15 12:24:23 +0530

    Delete TestPair_To_Number.h

--- TestPair_To_Number.h
+++
@@ -1,5 +0,0 @@
-#include"main.h"
-
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-ColorPair GetColorFromPairNumber(int pairNumber);
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
